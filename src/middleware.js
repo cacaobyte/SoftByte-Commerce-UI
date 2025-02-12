6d03cd81f2768bd7191d8a008e28b405939db407
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-02-12 06:41:09 -0600

    Android

--- src/middleware.js
+++ src/middleware.js
@@ -4,30 +4,22 @@ export function middleware(request) {
   const { pathname } = request.nextUrl;
   const token = request.cookies.get('token')?.value;
 
-  // Redirige desde la raíz a /Home/welcome
   if (pathname === '/') {
     return NextResponse.redirect(new URL('/Home/welcome', request.url));
   }
 
-  // Si ya tienes un token y tratas de ir a /auth/login, te redirige a Home
   if (token && pathname.startsWith('/auth/login')) {
-    console.log('Usuario autenticado, redirigiendo a Home...');
     return NextResponse.redirect(new URL('/Home/welcome', request.url));
   }
 
-  // Permite acceso a /auth/login si no tienes token
   if (!token && pathname.startsWith('/auth/login')) {
-    console.log('No autenticado, mostrando pantalla de login.');
     return NextResponse.next();
   }
 
-  // Verifica el token para las rutas protegidas
   if (!token) {
-    console.log('Token no encontrado, redirigiendo al login...');
     return NextResponse.redirect(new URL('/auth/login', request.url));
   }
 
-  console.log('Token encontrado, permitiendo acceso');
   return NextResponse.next();
 }
 
