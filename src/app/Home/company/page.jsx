b1fe8322eba0ee5add1f9a4c2067bb053d44454f
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-04-12 16:06:15 -0600

    validación para producción

---
+++ src/app/Home/company/page.jsx
@@ -0,0 +1,11 @@
+
+const companyPage = () => {
+    return (
+        <div>
+        <h1>Company</h1>
+        <p>Welcome to the company page!</p>
+        </div>
+    );
+    }
+
+    export default companyPage;
